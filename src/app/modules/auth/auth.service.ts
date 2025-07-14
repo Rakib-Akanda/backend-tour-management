b296b1ed695d4352c87eef52d67507a35bb6e783
Rakib-Akanda <rmd04037@gmail.com>
2025-07-14 17:02:58 +0600

    added google authentication with passportjs

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,9 +3,12 @@ import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import bcryptjs from "bcryptjs";
-import { generateToken } from "../../utils/jwt";
+import {
+  createNewAccessTokenWithRefreshToken,
+  createUserToken,
+} from "../../utils/userTokens";
+import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
-
 const credentialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
 
@@ -24,25 +27,64 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     throw new AppError(StatusCodes.BAD_REQUEST, "Incorrect Password");
   }
 
-  const jwtPayload = {
-    userId: isUserExist._id,
-    email: isUserExist.email,
-    role: isUserExist.role,
+  // const jwtPayload = {
+  //   userId: isUserExist._id,
+  //   email: isUserExist.email,
+  //   role: isUserExist.role,
+  // };
+  // //   const accessToken = jwt.sign(jwtPayload, "secret", { expiresIn: "1d" });
+  // const accessToken = generateToken(
+  //   jwtPayload,
+  //   envVars.JWT_ACCESS_SECRET,
+  //   envVars.JWT_ACCESS_EXPIRES
+  // );
+  // const refreshToken = generateToken(
+  //   jwtPayload,
+  //   envVars.JWT_REFRESH_SECRET,
+  //   envVars.JWT_REFRESH_EXPIRES
+  // );
+
+  const userToken = createUserToken(isUserExist);
+
+  // password hide korar jonno api and frontend er jonno
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: pass, ...rest } = isUserExist.toObject();
+
+  return {
+    accessToken: userToken.accessToken,
+    refreshToken: userToken.refreshToken,
+    user: rest,
   };
-  //   const accessToken = jwt.sign(jwtPayload, "secret", { expiresIn: "1d" });
-  const accessToken = generateToken(
-    jwtPayload,
-    envVars.JWT_ACCESS_SECRET,
-    envVars.JWT_ACCESS_EXPIRES
+};
+
+const getNewAccessToken = async (refreshToken: string) => {
+  const newAccessToken = await createNewAccessTokenWithRefreshToken(
+    refreshToken
   );
 
   return {
-    accessToken,
+    accessToken: newAccessToken,
   };
+};
+
+const resetPassword = async (oldPassword: string, newPassword: string, decodedToken: JwtPayload) => {
+  const user = await User.findById(decodedToken.userId)
+  if (!user) throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+
+  const isOldPasswordMatch = await bcryptjs.compare(oldPassword, user.password as string);
+  if (!isOldPasswordMatch) {
+    throw new AppError(StatusCodes.UNAUTHORIZED, "Old password does not match")
+  }
+  
+  user.password = await bcryptjs.hash(newPassword, Number(envVars.BCRYPT_SALT_ROUND));
+  await user.save()
+
 };
 
 // user - login - jwt token(email, role, _id) - booking / payment / payment or booking cancel-  token
 
 export const AuthServices = {
   credentialsLogin,
+  getNewAccessToken,
+  resetPassword
 };
